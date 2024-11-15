a35ee2b2f23a48a0a322d48f405ac8fb84b89721
ag777 <aaaggg777@qq.com>
2024-11-15 15:00:04 +0800

    修改 ScrollPaneUtils补充只有垂直或水平的滚动条包装的封装

--- src/main/java/github/ag777/common/tool/swing/util/ui/ScrollPaneUtils.java
+++ src/main/java/github/ag777/common/tool/swing/util/ui/ScrollPaneUtils.java
@@ -39,4 +39,32 @@ public class ScrollPaneUtils {
         // 返回包含视图组件的滚动面板
         return scrollPane;
     }
+
+    /**
+     * 将给定的视图组件包装在带有垂直滚动条的ScrollPane中
+     * 此方法适用于需要在垂直方向上扩展视图，而不希望出现水平滚动条的情况
+     *
+     * @param view 需要被包装的视图组件
+     * @return 返回一个带有垂直滚动条的ScrollPane
+     */
+    public static JScrollPane wrapVertical(Component view) {
+        JScrollPane scrollPane = wrap(view);
+        // 禁用水平滚动条
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        return scrollPane;
+    }
+
+    /**
+     * 将给定的视图组件包装在带有水平滚动条的ScrollPane中
+     * 此方法适用于需要在水平方向上扩展视图，而不希望出现垂直滚动条的情况
+     *
+     * @param view 需要被包装的视图组件
+     * @return 返回一个带有水平滚动条的ScrollPane
+     */
+    public static JScrollPane wrapHorizontal(Component view) {
+        JScrollPane scrollPane = wrap(view);
+        // 禁用垂直滚动条
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        return scrollPane;
+    }
 }
